55089b7029c87a662bd6f8666de02b4cb2733069
Vektor T13 <36127092+vektort13@users.noreply.github.com>
2019-03-09 16:24:48 +0200

    Update START.bat

--- START.bat
+++ START.bat
@@ -10,7 +10,7 @@
 	@echo You need to choose you Windows language:
 	@echo 1. Russian
 	@echo 2. Deutsch
+	@echo 3. English-USA
 set /P lsys="Enter Windows Language number: "
 netsh advfirewall import %~dp0firewall_rules_%lsys%.wfw
-netsh advfirewall reset
 @pause
